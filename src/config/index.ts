1ac058efa68b856c134a6c58a1c8a5052fe2a9fd
Bapi Biswas <bapi.biswas@webskitters.com>
2024-09-18 20:05:19 +0530

    working

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,8 +10,8 @@ export const PORT = process.env.PORT || 8000;
 export const JWT_SECRET = process.env.JWT_SECRET || 'mytoken';
 
 export const DB_CONNECTION = process.env.DB_CONNECTION || 'moongose';
-export const DB_HOST = process.env.DB_HOST || 'mongo2.webskitters.in';
-export const DB_PORT = process.env.DB_PORT || '27117';
-export const DB_DATABASE = process.env.DB_DATABASE || 'aaahealthcare';
-export const DB_USERNAME = process.env.DB_USERNAME || 'developer';
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'aaahealthcare@123$';
+export const DB_HOST = process.env.DB_HOST || 'localhost';
+export const DB_PORT = process.env.DB_PORT || '27017';
+export const DB_DATABASE = process.env.DB_DATABASE || 'node_backend';
+export const DB_USERNAME = process.env.DB_USERNAME || '';
+export const DB_PASSWORD = process.env.DB_PASSWORD || '';
